7342be0b70884c7f233a8f93e008f1f60e40afc4
Brady Buchholz <bradybuchholz@gmail.com>
2022-04-09 17:29:45 -0400

    app displays jokes after entering number of jokes and clicking get joke

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ function getJokes (e){
         if(this.status === 200){
             const response = JSON.parse(this.responseText);
             
+            let output = '';
+            if(response.type === 'success'){
+             response.value.forEach(function(joke){
+               output += `<li>${joke.joke}</li>`  
+             })   
+            }else {
+                output += `<li>Something went wrong</li>`;
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
         }
     }
 
